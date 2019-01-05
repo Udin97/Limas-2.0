80ff3ac5cedb373a6244827444e2e8ad00847794
sarafudin said <sarafudin97@gmail.com>
2019-01-05 12:30:53 +0800

    Add project files.

---
+++ Limas 2.0/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Limas_2._0.MvcApplication" Language="C#" %>
---
+++ Limas 2.0/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Limas_2._0.Startup))]
+namespace Limas_2._0
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
